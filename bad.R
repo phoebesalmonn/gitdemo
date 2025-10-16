d181b728bfbeb7c3bcbd15b4ac0c1a61aed44c4f
phoebesalmonn <phoebe.g.salmon@gmail.com>
2025-10-16 12:06:00 +0100

    changed first line

--- bad.R
+++ bad.R
@@ -1,10 +1,10 @@
 fun = function(one)
 {
-  one.plus.one <- oen + 1
-  four <- newVar <- matrix(1:10,nrow = 2)
+  one.plus.one <- one + 1
+  four <- matrix(1:10,nrow = 2)
   four[ 1, ]
   txt <- 'hi'
-  three <- two+ 1
+  three <- 2 + 1
   if(txt == 'hi') 4
   5}
 {
